6301fd8f846552327a2e8a324ee8157e70a7330f
Begi <123@gmail.com>
2023-01-01 15:46:38 +0500

    save

---
+++ main.js
@@ -0,0 +1,18 @@
+var imya = prompt('Введите своё имя');
+console.log('Меня зовут ' + imya);
+
+var age = +prompt('Введите свой возраст')
+console.log('Мне ' + age);
+
+var answer = +prompt('23 + 31 = ?');
+console.log('Ответ: 23 + 31 = ' + (23 + 31) + ' (Ваш ответ: ' + answer +')');
+var answer = +prompt('47 - 12 = ?');
+console.log('Ответ: 47 - 12 = ' + (47 - 12) + ' (Ваш ответ: ' + answer +')');
+var answer = +prompt('12 * 25 = ?');
+console.log('Ответ: 12 * 25 = ' + (12 * 25) + ' (Ваш ответ: ' + answer +')');
+var answer = +prompt('725 / 25 = ?');
+console.log('Ответ: 725 / 25 = ' + (725 / 25) + ' (Ваш ответ: ' + answer +')');
+var answer = +prompt('217 % 10 = ?');
+console.log('Ответ: 217 % 10 = ' + (217 % 10) + ' (Ваш ответ: ' + answer +')');
+
+alert('Спасибо!!');
